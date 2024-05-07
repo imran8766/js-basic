6e32ee0aec328ac4fd2b0652281a2e0dc6aa0d1a
MD IMRAN SHEIKH <166485094+imran8766@users.noreply.github.com>
2024-05-07 14:47:11 +0600

    yguyg

---
+++ hello.js
@@ -0,0 +1,13 @@
+console.log(21);
+console.log("Good Morning");
+var penpPrice = 20;
+console.log(penpPrice);
+var appleJuice = 25;
+console.log(typeof appleJuice);
+var name = "abdul sheikh";
+console.log(typeof name);
+var cond = true;
+console.log(typeof cond);
+var err = "My name is sheikh"
+console.log(err.toLocaleUpperCase());
+console.log(err.indexOf('is'));
